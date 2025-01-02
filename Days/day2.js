0444044d0287d3176774b4c8bbddd749ecfb06cb
VijendraChouhanDev <vijendras12340@gmail.com>
2025-01-02 09:51:13 +0530

    Day two practice assignments

---
+++ Days/day2.js
@@ -0,0 +1,89 @@
+function compareTwoNumbers(num1, num2) {
+  // Write code here
+  console.log(num1 > num2);
+  console.log(num1 < num2);
+  console.log(num1 == num2);
+}
+
+function cubeAndSquare(n, m) {
+  //write code here
+  let cube = n ** 3;
+  let square = m ** 2;
+  console.log(cube > square);
+}
+
+function areaAndPerimeter(L1, B1, L2, B2) {
+  //write code here
+  let area = L1 * B1;
+  let area2 = L2 * B2;
+  let perimeter = 2 * (L1 + B1);
+  let perimeter2 = 2 * (L2 + B2);
+  console.log(area > area2);
+  console.log(perimeter > perimeter2);
+}
+
+function sumAndCompare(one, two, three, four, five) {
+  // Write code here
+  let sum1 = one + two + three;
+  let sum2 = four + five;
+  console.log(sum1 > sum2);
+}
+
+function updateAndCompare(num1, num2, num3) {
+  // Write code here
+  console.log(num1 > num2);
+  num1 = num1 + num3;
+  console.log(num1 > num2);
+}
+
+function infinityStones(one, two, three, four, five, six) {
+  //Write code here
+  let doubled = two * 2 + five * 2 + six * 2;
+  let triple = three * 3 + four * 3;
+  console.log(one + doubled + triple);
+}
+
+function findX(x) {
+  // Write code here
+  console.log(x * 7 * 7);
+}
+
+function compareSevenNumbers(a, b, c, d, e, f, g) {
+  // Write code here
+  let sum1 = (a + b) * c;
+  let sum2 = d + e + f + g;
+  console.log(sum1 > sum2);
+}
+
+function printMasaiSchool() {
+  //write your code here
+  console.log("Masai School");
+  console.log("A Transformation in Education");
+}
+
+function multiplyBy50(number) {
+  // Write code here
+  console.log(number * 50);
+}
+function operationOnNumbers(number) {
+  // Write code here
+  let res = number * 3 + 7 - 10;
+  console.log(res);
+}
+function squareIt(N) {
+  // Write code here
+  console.log(N ** 2);
+}
+function findProduct(A, B, C, D, E) {
+  //write your code here
+  console.log(A * B * C * D * E);
+}
+function doubleAll(one, two, three, four, five) {
+  // Write code here
+  console.log(one * 2 + two * 2 + three * 2 + four * 2 + five * 2);
+}
+
+function doubleTwoTripleOne(one, two, three, four) {
+  // Write code here
+  console.log(one * 2 + two * 2 + three * 3 + four + 4);
+}
